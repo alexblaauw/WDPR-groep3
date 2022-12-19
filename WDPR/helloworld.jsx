6b36bebad2d91b33da4f6bc9e015b1b415a636e6
alexblaauw <alexblaauw44@gmail.com>
2022-12-19 10:32:54 +0100

    React Toevoeging

---
+++ WDPR/helloworld.jsx
@@ -0,0 +1,10 @@
+﻿
+class HelloWorld extends React.Component {
+    render() {
+        return (<h1>My First ReactJS MVC App!</h1>);
+    }
+}
+ReactDOM.render(
+    <HelloWorld />,
+    document.getElementById('dvMessage')
+); 

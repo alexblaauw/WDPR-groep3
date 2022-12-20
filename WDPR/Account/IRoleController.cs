388100444ba84938e7bb17ab40fb07be4825a239
alexblaauw <alexblaauw44@gmail.com>
2022-12-20 10:34:33 +0100

    Account Kern

---
+++ WDPR/Account/IRoleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ReactApp.Account
+{
+    // KIJK UIT: classes die een controller extenden moeten ook ControllerBase extenden
+    public interface IRoleController
+    {
+        public Task<object> RegistreerRol([FromBody] string rolNaam); // Moet async zijn
+        public IEnumerable<IdentityRole> GetRol([FromBody] string rolNaam);
+    }
+}
